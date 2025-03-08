d9153ace0eb1e522539c5e16a507497265d6a2a2
daniel21 <mitracheadaniel@gmail.com>
2025-03-08 11:35:21 +0000

    added comments + read inputs from file

--- automat.py
+++ automat.py
@@ -3,6 +3,9 @@ import re
 with open("config_automat.txt", "r") as file:
     config_data = file.read()
 
+# Preprocessing the data (removing comments)
+config_data = re.sub(r"#.*", "", config_data)
+
 patterns = {
     "states": r"\[States\](.*?)\[/States\]",
     "sigma": r"\[Sigma\](.*?)\[/Sigma\]",
@@ -15,7 +18,6 @@ extracted_data = {}
 
 for key, pattern in patterns.items():
     extracted_data[key] = re.findall(pattern, config_data, re.DOTALL)[0].strip()
-    print(f"{key}: {extracted_data[key]}")
 
 # Extracting states
 states = extracted_data["states"].split(",")
@@ -51,3 +53,10 @@ for letter in sigma:
 for t in transitions:
     rules[t[1]][t[0]] = t[2]
 
+with open("input.txt", "r") as file:
+    input_data = file.read()
+
+# Preprocessing the data (removing comments)
+input_data = re.sub(r"#.*", "", input_data)
+
+inputs = input_data.split("\n")
