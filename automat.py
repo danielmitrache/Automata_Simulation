7da5c672f3a158db12a63bb60c7893cc3458010e
daniel21 <mitracheadaniel@gmail.com>
2025-03-08 12:08:02 +0000

    added input and 2 functions for automat object

--- automat.py
+++ automat.py
@@ -1,62 +1,86 @@
 import re
 
-with open("config_automat.txt", "r") as file:
-    config_data = file.read()
+def get_config_data(config_file):
+    with open(config_file, "r") as file:
+        config_data = file.read()
 
-# Preprocessing the data (removing comments)
-config_data = re.sub(r"#.*", "", config_data)
+    # Preprocessing the data (removing comments)
+    config_data = re.sub(r"#.*", "", config_data)
 
-patterns = {
-    "states": r"\[States\](.*?)\[/States\]",
-    "sigma": r"\[Sigma\](.*?)\[/Sigma\]",
-    "transitions": r"\[Transitions\](.*?)\[/Transitions\]",
-    "start": r"\[Start\](.*?)\[/Start\]",
-    "final": r"\[Final\](.*?)\[/Final\]"
-}
+    patterns = {
+        "states": r"\[States\](.*?)\[/States\]",
+        "sigma": r"\[Sigma\](.*?)\[/Sigma\]",
+        "transitions": r"\[Transitions\](.*?)\[/Transitions\]",
+        "start": r"\[Start\](.*?)\[/Start\]",
+        "final": r"\[Final\](.*?)\[/Final\]"
+    }
 
-extracted_data = {}
+    extracted_data = {}
 
-for key, pattern in patterns.items():
-    extracted_data[key] = re.findall(pattern, config_data, re.DOTALL)[0].strip()
+    for key, pattern in patterns.items():
+        extracted_data[key] = re.findall(pattern, config_data, re.DOTALL)[0].strip()
 
-# Extracting states
-states = extracted_data["states"].split(",")
-states = [state.strip() for state in states]
+    # Extracting states
+    states = extracted_data["states"].split(",")
+    states = [state.strip() for state in states]
 
-# Extracting sigma
-sigma = extracted_data["sigma"].split(",")
-sigma = [s.strip() for s in sigma]
+    # Extracting sigma
+    sigma = extracted_data["sigma"].split(",")
+    sigma = [s.strip() for s in sigma]
 
-# Extracting transitions
-transitions_unp = extracted_data["transitions"].split(";")
-transitions_unp = [t.strip() for t in transitions_unp]
-transitions_unp = [tuple(t.split(",")) for t in transitions_unp]
-transitions = []
-for t in transitions_unp:
-    tpl = []
-    for i in t:
-        tpl.append(i.strip())
-    transitions.append(tuple(tpl)) if len(tpl) == 3 else None
+    # Extracting transitions
+    transitions_unp = extracted_data["transitions"].split(";")
+    transitions_unp = [t.strip() for t in transitions_unp]
+    transitions_unp = [tuple(t.split(",")) for t in transitions_unp]
+    transitions = []
+    for t in transitions_unp:
+        tpl = []
+        for i in t:
+            tpl.append(i.strip())
+        transitions.append(tuple(tpl)) if len(tpl) == 3 else None
 
-# Extracting start state
-start = extracted_data["start"].strip()
+    # Extracting start state
+    start = extracted_data["start"].strip()
 
-# Extracting final states
-final = extracted_data["final"].split(",")
-final = [f.strip() for f in final]
+    # Extracting final states
+    final = extracted_data["final"].split(",")
+    final = [f.strip() for f in final]
 
 
-# Making a dictionary (key = letter) of dictionaries (key = current state) of rules (current state -> next state)
-rules = {}
-for letter in sigma:
-    rules[letter] = {}
-for t in transitions:
-    rules[t[1]][t[0]] = t[2]
+    # Making a dictionary (key = letter) of dictionaries (key = current state) of rules (current state -> next state)
+    rules = {}
+    for letter in sigma:
+        rules[letter] = {}
+    for t in transitions:
+        rules[t[1]][t[0]] = t[2]
 
-with open("input.txt", "r") as file:
-    input_data = file.read()
+    return states, sigma, rules, start, final
 
-# Preprocessing the data (removing comments)
-input_data = re.sub(r"#.*", "", input_data)
+class Automat:
+    def __init__(self, states, sigma, transitions, start, final):
+        self.states = states
+        self.sigma = sigma
+        self.transitions = transitions
+        self.start = start
+        self.final = final
 
-inputs = input_data.split("\n")
+    def simulate_automat(self, input_data):
+        state = self.start
+        for cnt, letter in enumerate(input_data[::-1]):
+            
+            if cnt == len(input_data) - 1:
+                print(state, end="\n")
+            else:
+                print(state + ' -> ', end="")
+
+            if letter not in self.sigma:
+                raise ValueError("Invalid input")
+            state = self.transitions[letter][state]
+        
+    def is_accepted(self, input_data):
+        state = self.start
+        for letter in input_data[::-1]:
+            if letter not in self.sigma:
+                return False
+            state = self.transitions[letter][state]
+        return state in self.final
---
+++ main.py
@@ -0,0 +1,13 @@
+from automat import Automat, get_config_data
+
+INPUT_FILE = "input.txt"
+CONFIG_FILE = "config_automat.txt"
+
+with open(INPUT_FILE, "r") as file:
+    input_data = file.read()
+
+automat = Automat(*get_config_data(CONFIG_FILE))
+
+for input in input_data.split("\n"):
+    automat.simulate_automat(input)
+    print(automat.is_accepted(input))
